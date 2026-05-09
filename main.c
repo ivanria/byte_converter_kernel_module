8513b9176b89bec6076e03b1c852de8505e246fc
vseslav-dev <3511345+vseslav-dev@users.noreply.github.com>
2026-05-09 15:07:17 +0300

    modify init: rewrite with goto, sanitize exit

--- main.c
+++ main.c
@@ -32,14 +32,33 @@ static ssize_t dev_read(struct file *file, char __user *buf, size_t count, loff_
 }
 
 // open/close callback
+/*
+struct dev_context {
+	char *buffer;
+	size_t data_size;
+};
+*/
+
 static int dev_open(struct inode *inode, struct file *file)
 {
+	/*
+	struct dev_context *ctx = kzalloc(sizeof(*ctx), GFP_KERNEL);
+	if (!ctx) return -ENOMEM;
+
+	ctx->buffer = kmalloc(MAIN_UBUF_SIZE, GFP_KERNEL);
+	if (!ctx->buffer) {
+		kfree(ctx);
+		return -ENOMEM;
+	}
+
+	file->private_data = ctx; // Привязываем контекст к файлу
+	*/
 	return 0;
 }
 
 // Global visible variables
-char *byte_conv_dev_fname = "byte_converter";
-char *byte_conv_proc_fname = "byte_conv_mask";
+const char * const byte_conv_dev_fname = "byte_converter";
+const char * const byte_conv_proc_fname = "byte_conv_mask";
 u16 byte_conv_mask;
 
 /*******************************************/
@@ -71,51 +90,63 @@ static int __init converter_init(void)
 
 	ret = alloc_chrdev_region(&dev_num, 0, 1, byte_conv_dev_fname);
 	if (ret < 0) {
-		pr_warn("Can't create /dev/%s file\n", byte_conv_dev_fname);
+		pr_warn("Can't alloc_chrdev_region\n");
 		return ret;
 	}
 
-	BC_PR_DEBUG("Major num is: %u, Minor num is: %u\n",
-			MAJOR(dev_num), MINOR(dev_num));
-
 	byte_conv_class = class_create(byte_conv_dev_fname);
 	if (IS_ERR(byte_conv_class)) {
-		unregister_chrdev_region(dev_num, 1);
 		pr_warn("Can't create /dev/%s file\n", byte_conv_dev_fname);
-		return PTR_ERR(byte_conv_class);
+		ret = PTR_ERR(byte_conv_class);
+		goto ERR_UNREG_REGION;
 	}
 
 	cdev_init(&byte_conv_cdev, &fops);
 	byte_conv_cdev.owner = THIS_MODULE;
-	cdev_add(&byte_conv_cdev, dev_num, 1);
+	ret = cdev_add(&byte_conv_cdev, dev_num, 1);
+	if (ret < 0) {
+		pr_warn("Can't cdev_add() dev_num MAJ: %u, MIN: %u\n",
+				MAJOR(dev_num), MINOR(dev_num));
+		goto ERR_DESTROY_CLASS;
+	}
 
 	byte_conv_device = device_create(byte_conv_class, NULL, dev_num, NULL,
 			"%s", byte_conv_dev_fname);
 	if (IS_ERR(byte_conv_device)) {
-		class_destroy(byte_conv_class);
-		unregister_chrdev_region(dev_num, 1);
 		pr_warn("Can't create /dev/%s file\n", byte_conv_dev_fname);
-		return PTR_ERR(byte_conv_device);
+		goto ERR_CDEV_DEL;
 	}
 
-	entry = proc_create
-		(byte_conv_proc_fname, 0666, NULL, &byte_conv_mask_ops);
+	entry = proc_create(byte_conv_proc_fname,
+			0666, NULL, &byte_conv_mask_ops);
 	if (!entry) {
 		pr_warn("Can't create /proc/%s file\n", byte_conv_proc_fname);
-		return -ENOMEM;
+		ret = -ENOMEM;
+		goto ERR_DEVICE_DESTROY;
 	}
 
+	BC_PR_DEBUG("Major num is: %u, Minor num is: %u\n",
+			MAJOR(dev_num), MINOR(dev_num));
 	return 0;
+
+ERR_DEVICE_DESTROY:
+    device_destroy(byte_conv_class, dev_num);
+ERR_CDEV_DEL:
+    cdev_del(&byte_conv_cdev);
+ERR_DESTROY_CLASS:
+    class_destroy(byte_conv_class);
+ERR_UNREG_REGION:
+    unregister_chrdev_region(dev_num, 1);
+    return ret;
 }
 
 static void __exit converter_exit(void)
 {
-	device_destroy(byte_conv_class, MKDEV(MAJOR(dev_num), MINOR(dev_num)));
+	remove_proc_entry(byte_conv_proc_fname, NULL);
+	device_destroy(byte_conv_class, dev_num);
 	cdev_del(&byte_conv_cdev);
-	class_unregister(byte_conv_class);
 	class_destroy(byte_conv_class);
 	unregister_chrdev_region(dev_num, 1);
-	remove_proc_entry(byte_conv_proc_fname, NULL);
 	pr_info("Char Device Converter module is unloaded\n");
 }
 
